6d50a6a2702e9c8d928b07a8653c00b5be0c8656
mooenz <joss92821@hotmail.com>
2022-05-01 15:08:13 -0500

    add new function close menu mobile

--- edie-homepage-master/index.js
+++ edie-homepage-master/index.js
@@ -1,3 +1,4 @@
+//Listeners for burger button
 const $burger = document.querySelector('.burger');
 const $list = document.querySelector('.nav__links');
 
@@ -7,3 +8,16 @@ const changeBurger = () => {
 };
 
 $burger.addEventListener('click', changeBurger, false);
+
+//Listeners for nav list
+
+const $itemsNode = document.querySelectorAll('.nav__link');
+const $items = [...$itemsNode];
+
+$items.map((item) => {
+  const closeMenu = () => {
+    $list.classList.remove('showList');
+  }
+
+  item.addEventListener("click", closeMenu, false)
+})

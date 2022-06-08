b65004945e375d22d97e445611c3880f94ad5fbf
mooenz <joss92821@hotmail.com>
2022-06-08 18:33:08 -0500

    add new structure scss files

--- 404-not-found-master/src/index.js
+++ 404-not-found-master/src/index.js
@@ -1,16 +1,16 @@
 const app = document.querySelector('.app');
 
 app.innerHTML = `
-  <p class="app__logo">404 not found</p>
-  <div class="app__container">
-    <img class="app__img" src="./assets/Scarecrow.png" />
-    <div class="app__body">
-      <h1 class="app__title">I have bad news for you</h1>
-      <p class="app__paragraph">
+  <p class="logo">404 not found</p>
+  <div class="container">
+    <img class="img" src="./assets/img/Scarecrow.png" />
+    <div class="body">
+      <h1 class="title">I have bad news for you</h1>
+      <p class="paragraph">
         The page you are looking for might be removed or is temporarily unavailable
       </p>
-      <button class="app__button">Back to homepage</button>
+      <button class="button">Back to homepage</button>
     </div>
   </div>
-  <p class="app__footer">created by <a href="https://www.mooenz.me/" class="strong">Mooenz</a> - devChallenges.io</p>
+  <p class="footer">created by <a href="https://www.mooenz.me/" class="strong">Mooenz</a> - devChallenges.io</p>
 `;
